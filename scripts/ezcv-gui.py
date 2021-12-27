05e2accfe1c0e885a34f504e33e8a43e1b560210
Frederico Tommasi Caroli <fredtcaroli@gmail.com>
2021-12-27 12:56:38 -0800

    Moving operators to ezcv_operators

--- scripts/ezcv-gui.py
+++ scripts/ezcv-gui.py
@@ -1,4 +1,5 @@
 import sys
+import importlib_metadata
 
 from PyQt6.QtWidgets import QApplication
 
@@ -6,9 +7,17 @@ from ezcv_gui.main import EzCV
 
 
 def main():
+    load_operators()
     app = QApplication([])
     main_widget = EzCV()
     sys.exit(app.exec())
+
+
+def load_operators():
+    entry_points = importlib_metadata.entry_points(group='ezcv_operators')
+    for entry_point in entry_points:
+        print(f'Loading operators library "{entry_point.name}"')
+        entry_point.load()
 
 
 if __name__ == '__main__':
